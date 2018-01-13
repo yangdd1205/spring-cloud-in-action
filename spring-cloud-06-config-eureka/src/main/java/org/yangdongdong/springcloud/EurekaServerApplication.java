73030cd4673819726c35ea8bf282cabb1aa17056
yangdd1205 <yangdd1205@126.com>
2018-01-13 20:14:48 +0800

    :sparkles: spring cloud config

--- spring-cloud-06-config-eureka/src/main/java/org/yangdongdong/springcloud/EurekaServerApplication.java
+++ spring-cloud-06-config-eureka/src/main/java/org/yangdongdong/springcloud/EurekaServerApplication.java
@@ -2,13 +2,13 @@ package org.yangdongdong.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
+@EnableEurekaServer
 @SpringBootApplication
-public class Application {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
